331da9799cff1ab2840c90d378426c9eec1f4681
Ruinwalker7 <973521624@qq.com>
2023-07-15 15:35:30 +0800

    feat: 模型推理 进行排序

--- camera/src/camera.cpp
+++ camera/src/camera.cpp
@@ -1,10 +1,102 @@
 #include <cstdio>
+#include <memory>
+#include <sensor_msgs/msg/image.hpp>
+#include "rclcpp/rclcpp.hpp"
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <string>
+#include <cv_bridge/cv_bridge.h>
 
-int main(int argc, char ** argv)
+class ImagePublisher : public rclcpp::Node
 {
-  (void) argc;
-  (void) argv;
+public:
+  ImagePublisher(const rclcpp::NodeOptions &options) : Node("image_publisher", options)
+  {
+    RCLCPP_INFO(this->get_logger(), "Initializing Camera Driver!");
+    publisher_ = this->create_publisher<sensor_msgs::msg::Image>("raw_img", 1);
+    node_clock = this->get_clock();
 
-  printf("hello world camera package\n");
-  return 0;
-}
+    // Check if camera is alive every 100ms.
+    timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(10), std::bind(&ImagePublisher::timerCallback, this));
+  }
+
+  void timerCallback()
+  {
+    while (!is_open_ && rclcpp::ok())
+    {
+      open();
+      rclcpp::sleep_for(std::chrono::seconds(1));
+    }
+
+    // watch dog
+    // double dt = (this->now().seconds() - rclcpp::Time(camera_info_.header.stamp).seconds());
+    // if (dt > 2.0)
+    // {
+    //   RCLCPP_ERROR(this->get_logger(), "Frame has been lost for %f seconds, restart!", dt);
+    //   this->close();
+    //   return;
+    // }
+    // set camera exposure
+    // int temp_exposure_time = this->get_parameter("exposure_time").as_int();
+    // double temp_gain = this->get_parameter("gain").as_double();
+    // if (abs(temp_exposure_time - exposure_time_) > 0.5 || abs(temp_gain - gain_) > 0.05)
+    // {
+    //   exposure_time_ = temp_exposure_time;
+    //   gain_ = temp_gain;
+    //   RCLCPP_INFO(this->get_logger(), "Set exposure_time: %d, gain: %f", exposure_time_, gain_);
+    // }
+
+    cv::Mat camera_img;
+    capture1.read(camera_img);
+    // std::cout << camera_img.size() << std::endl;
+    cv::Mat mat;
+    cv::resize(camera_img,mat,cv::Size(1280,720));
+    // sensor_msgs::msg::Image::SharedPtr msg =
+    // cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8",mat).toImageMsg();
+    image_msg_ = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", camera_img).toImageMsg();
+    image_msg_->header.stamp = node_clock->now();
+    publisher_->publish(*image_msg_.get());
+
+    cv::imshow("camera", camera_img);
+    cv::waitKey(1);
+  }
+
+  void open()
+  {
+    capture1.open(-1);
+
+    capture1.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+    capture1.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+    capture1.set(cv::CAP_PROP_FRAME_WIDTH, 1280);
+    capture1.set(cv::CAP_PROP_FPS, 60);
+    capture1.set(cv::CAP_PROP_AUTO_EXPOSURE, 0.25); // 亮度 1
+
+    int height = capture1.get(cv::CAP_PROP_FRAME_HEIGHT);
+    int weight = capture1.get(cv::CAP_PROP_FRAME_WIDTH);
+    RCLCPP_INFO(this->get_logger(), "%d %d", height, weight);
+
+    if (!capture1.isOpened())
+    {
+      is_open_ = false;
+      RCLCPP_INFO(this->get_logger(), "Camera Error!");
+    }
+    else
+    {
+      is_open_ = true;
+      RCLCPP_INFO(this->get_logger(), "Camera opened");
+    }
+  }
+
+private:
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+  sensor_msgs::msg::Image::SharedPtr image_msg_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Clock::SharedPtr node_clock;
+  bool is_open_;
+
+  cv::VideoCapture capture1;
+};
+
+#include "rclcpp_components/register_node_macro.hpp"
+RCLCPP_COMPONENTS_REGISTER_NODE(ImagePublisher)
--- detector/detector/detector.py
+++ detector/detector/detector.py
@@ -6,8 +6,9 @@ import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import String
+import torch
 
-model = YOLO('/home/chen/Documents/crane/detector/model/best.pt')  # load a custom model
+model = YOLO('/home/chen/Documents/crane/detector/model/best.pt')
 
 class ImageSubscriber(Node):
 
@@ -16,16 +17,42 @@ class ImageSubscriber(Node):
         self.subscription = self.create_subscription(
             Image,
             'raw_img',
-            self.imageCallback,10)
+            self.imageCallback,1)
         self.subscriptions
+        self.cvbridge = CvBridge()
 
     def imageCallback(self,msg):
-        cv_img = CvBridge.imgmsg_to_cv2(msg, "bgr8")
-        cv2.imshow("frame" , cv_img)
-        cv2.waitKey(3)
-        results = model(cv_img)
-        print(results)
-    
+        cv_img = self.cvbridge.imgmsg_to_cv2(msg, "bgr8")
+        results = model.predict(cv_img,conf=0.35)
+        res_plotted = results[0].plot()
+        cv2.imshow("result", res_plotted)
+        cv2.waitKey(1)
+
+        xyxy = results[0].boxes.xyxy
+        cls = results[0].boxes.cls
+        if len(xyxy) != 6:
+            return
+        cls=cls.reshape(len(cls),1)
+        c = torch.cat([xyxy,cls],dim=1)
+        c = c.sort(0,False)[0]
+        print(c)
+
+        
+        # for i in range(0,6):
+        #     xyxy[i]=cls[i]
+        # print(xyxy)
+        # print(type(boxes))
+        # print(boxes)
+
+        # rect = torch.tensor()
+        # for box in boxes:
+        #     print(box.xyxy)
+        #     print(box.cls)
+        # print("--------------")
+        # print(results[0])
+
+
+
 def main():
     rclpy.init()
     # print('Hi from detector.')
